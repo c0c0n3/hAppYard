adb690424b52fc532d04eec394a6b528f62a2b39
andrea falconi <andrea.falconi@gmail.com>
2015-10-10 19:57:42 +0200

    cocone-backup: implement deployment script.

--- cocone-backup/deploy.sh
+++ cocone-backup/deploy.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+TARGET_DIR=/Volumes/data/_sync_
+
+mkdir -p $TARGET_DIR/bin
+mkdir -p $TARGET_DIR/config
+mkdir -p $TARGET_DIR/log
+
+cp bin/*.sh $TARGET_DIR/bin/
+chmod +x $TARGET_DIR/bin/*.sh
+
+cp config/* $TARGET_DIR/config/
+
